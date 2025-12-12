ae6e1037cc74ae9812778bafb7aa30691a367310
Viwu <sinconsistencia@gmail.com>
2025-12-12 10:35:05 -0600

    choice txt

--- source/helus/scenes/intro/MakeAChoice.hx
+++ source/helus/scenes/intro/MakeAChoice.hx
@@ -1,17 +1,20 @@
 package helus.scenes.intro;
 
+import flixel.math.FlxPoint;
 import helus.backend.TextManager;
 import flixel.text.FlxText;
 import flixel.FlxState;
 
 class MakeAChoice extends FlxState
 {
-    public var helloText:FlxText= TextManager.makeTranslatedText('hello [[user]]');
+	public var helloText:FlxText = TextManager.makeTranslatedText('hello [[user]]', FlxPoint.get(20, 20));
+	public var makeAChoice:FlxText = TextManager.makeTranslatedText('make a choice', FlxPoint.get(480, 270));
 
 	override function create()
 	{
 		super.create();
 
-        add(helloText);
+		add(helloText);
+		add(makeAChoice);
 	}
 }
