f222617e7ea8f28be595865869b4595acf63de0f
Viwu <sinconsistencia@gmail.com>
2025-12-12 10:23:14 -0600

    getLine

--- source/helus/languages/LanguageManager.hx
+++ source/helus/languages/LanguageManager.hx
@@ -3,6 +3,8 @@ package helus.languages;
 import lime.utils.Assets;
 import haxe.Json;
 
+using StringTools;
+
 class LanguageManager
 {
 	public static var language:String = 'en-US';
@@ -10,6 +12,16 @@ class LanguageManager
 
 	public static function getLine(line:String, ?fallback:String):String
 	{
+		if (languageFile == null)
+			return fallback;
+
+		line = line.replace(' ', '').replace('\n', '-').replace('\t', '_');
+
+		if (Reflect.hasField(languageFile.lines, line))
+			return Reflect.field(languageFile.lines, line);
+		else
+			trace('Missing language file line: ' + line);
+
 		return fallback;
 	}
 
