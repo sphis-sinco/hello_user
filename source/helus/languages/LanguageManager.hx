ea7f44e5978f9760f6a273745e305b17ae48f614
Viwu <sinconsistencia@gmail.com>
2025-12-12 10:21:10 -0600

    reload language

--- source/helus/languages/LanguageManager.hx
+++ source/helus/languages/LanguageManager.hx
@@ -1,9 +1,20 @@
 package helus.languages;
 
+import lime.utils.Assets;
+import haxe.Json;
+
 class LanguageManager
 {
-    public static var LANGUAGE:LanguageFile;
+	public static var language:String = 'en-US';
+	public static var languageFile:LanguageFile;
 
-	public static function getLine(line:String, ?fallback:String):String { return fallback; }
-	public static function reloadLanguage() {}
+	public static function getLine(line:String, ?fallback:String):String
+	{
+		return fallback;
+	}
+
+	public static function reloadLanguage()
+	{
+		languageFile = Json.parse(Assets.getText('assets/languages/$language.json'));
+	}
 }
