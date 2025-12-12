cb86a3c22803a8a799a69b7b469d1e8c6521f630
Viwu <sinconsistencia@gmail.com>
2025-12-12 10:36:23 -0600

    convert changes

--- source/helus/languages/LanguageManager.hx
+++ source/helus/languages/LanguageManager.hx
@@ -15,7 +15,7 @@ class LanguageManager
 		if (languageFile == null)
 			return fallback ?? line;
 
-		var convertedLine = line.toLowerCase().replace(' ', '').replace('\n', '-').replace('\t', '_');
+		var convertedLine = line.toLowerCase().replace(' ', '-').replace('\n', '_').replace('\t', '|');
 
 		if (Reflect.hasField(languageFile.lines, convertedLine))
 			return Reflect.field(languageFile.lines, convertedLine);
